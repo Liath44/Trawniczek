55bf4c575345297d50bc9e70c5ec1dd7890a3886
liath44 <maciek_dra@wp.pl>
2020-03-25 23:25:28 +0100

    slowly starting phase 2 of gardener.c

--- ProposedDesign/gardener.c
+++ ProposedDesign/gardener.c
@@ -21,7 +21,49 @@ struct _reclist
 typedef struct _reclist reclist;
 */
 
-pointlist *NewPoint()
+void ResetSignsJump(char **Lawn, int xsize, int ysize)
+	{
+	for(int j = 0; j < ysize; j++)
+		{
+		for(int i = 0; i < xsize; i++)
+			*(*(Lawn+JUMP*i)+JUMP*j) = -(*(*(Lawn+JUMP*i)+JUMP*j));
+		}
+	}
+
+void SignAreaJump(char **Lawn, int xsize, int ysize, int i, int j)
+	{
+	*(*(Lawn+JUMP*i)+JUMP*j) = -(*(*(Lawn+JUMP*i)+JUMP*j));
+	if(i + 1 < xsize && *(*(Lawn+JUMP*(i+1))+JUMP*j) == 1)
+		SignAreaJump(Lawn, xsize, ysize, i + 1, j);
+	if(i - 1 >= 0 && *(*(Lawn+JUMP*(i-1))+JUMP*j) == 1)
+		SignAreaJump(Lawn, xsize, ysize, i - 1, j);
+	if(j + 1 < ysize && *(*(Lawn+JUMP*i)+JUMP*(j+1)) == 1)
+		SignAreaJump(Lawn, xsize, ysize, i, j + 1);
+	if(j - 1 >= 0 && *(*(Lawn+JUMP*i)+JUMP*(j-1)) == 1)
+		SignAreaJump(Lawn, xsize, ysize, i, j - 1);
+	}
+
+pointlist *NewPoint(int x, int y, pointlist *next)
+	{
+	pointlist *outcome = malloc(sizeof(*outcome));
+	if(outcome == NULL)
+		return NULL;
+	outcome -> x = x;
+	outcome -> y = y;
+	outcome -> next = next;
+	return outcome;
+	}
+
+void FreePoints(pointlist *point)
+	{
+	pointlist *piv;
+	while(point != NULL)
+		{
+		piv = point -> next;
+		free(point);
+		point = piv;
+		}
+	}
 
 pointlist *FindAreas(char **Lawn, int xsize, int ysize)
 	{
@@ -34,20 +76,52 @@ pointlist *FindAreas(char **Lawn, int xsize, int ysize)
 				{
 				pointlist *piv = NewPoint(i, j, ret);
 				if(piv == NULL)
+					{
+					FreePoints(ret);
 					return NULL;
+					}
 				ret = piv;
-				SignArea(Lawn, xsize, ysize, i, j);
+				SignAreaJump(Lawn, xsize, ysize, i, j);
 				}
 			}
 		}
-	ResetSigns(Lawn, xsize, ysize);
+	ResetSignsJump(Lawn, xsize, ysize);
 	return ret;
+	}
+
+void FreeRectangles(reclist *rec)
+	{
+	reclist *piv;
+	while(rec != NULL)
+		{
+		piv = rec -> next;
+		free(rec);
+		rec = piv;
+		}
+	}
+
+reclist *DivideRectangle(char **Lawn, int x, int y, int xsize, int ysize)
+	{
+	//
 	}
 
 int DoTheJob(char **Lawn, parameters *Param, sprlist *Sprinklers);
 	{
 	pointlist *areas = FindAreas(Lawn, Param->xsize/JUMP, Param->ysize/JUMP);
+	pointlist *pivareas = areas;
 	if(areas == NULL)
 		return 0;
-	
+	while(areas != NULL)
+		{
+		reclist *rectangles = DivideRectangle(Lawn, areas->x, areas->y, Param->xsize/JUMP, Param->ysize/JUMP);
+		if(rectangles == NULL)
+			{
+			FreePoints(pivareas);
+			return 0;
+			}
+		FreeRectangles(rectangles);
+		areas = areas -> next;
+		}
+	FreePoints(pivareas);
+	return 1;	
 	}
