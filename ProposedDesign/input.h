e66e1997c15972008d2dd37af8b1a77f1ee67dd7
Norsienk <smola27ms@gmail.com>
2020-04-02 22:52:23 +0100

    cleaning up, i hope

--- ProposedDesign/input.h
+++ ProposedDesign/input.h
@@ -7,30 +7,53 @@
 
 /*
  * Transforms information from lawnfile into matrix
- * (0 - non-lawn, 1...n - water level - 1). 
+ * (0 - non-lawn, 1...n - water level - 1).
  * Updates Param structure with necessary information.
- * 
+ *
  * FILE *lawnfile - stores to-be-transformed lawn.
  * Desired format is detailed in documentation enclosed
- * 
- * parameters *Param - points to structure that holds 
+ *
+ * parameters *Param - points to structure that holds
  * lawn's parameters. Function updates fields of this structure
- * 
- * Returns NULL on failure
- * 
- * Returns pointer to created matrix on success.
+ *
+ * char **lawn - points to to be crated matrix
+ *
+ * Returns error code on failure
+ *
+ * Returns 0 on success.
  */
-char **CreateLawn(FILE *lawnfile, parameters *Param);
+int CreateLawn(FILE *lawnfile, parameters *Param, char **lawn);
 
 /*
  * Initializes Param structure with necessary information
- * 
+ *
  * FILE *lawnfile - stores information on subject lawn.
  * Desired format is detailed in documentation enclosed
- * 
- * parameters *Param - points to structure that holds 
+ *
+ * parameters *Param - points to structure that holds
  * lawn's parameters. Function initializes fields of this structure
+ *
+ * int time - value form argv[3]. Defaults to 100. It is added to
+ * the Param structure
+ *
+ * Returns error code on failure
+ *
+ * Returns 0 on success.
  */
-void InitializeParameters(FILE *lawnfile, parameters *Param);
+int InitializeParameters(FILE *lawnfile, parameters *Param, int time);
+
+/*
+ * Fills the desired square on matrix with corresponding values
+ *
+ * char **lawn - pointer to the matrix that will be filled
+ *
+ * int x, int y - coordinates of the top left corner of the
+ * square
+ *
+ * char c - value that will be used to fill the square
+ *
+ * int pixel - the side length on the square. Taken from Param
+ */
+void fillSquare(char **lawn, int x, int y, char c, int pixel);
 
 #endif
